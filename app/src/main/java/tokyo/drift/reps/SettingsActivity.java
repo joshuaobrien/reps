8f1cf6c83fb970d33a37771a1177991f4730ff6a
tiget <jjobrien12@gmail.com>
2017-03-02 13:21:57 +1000

    Added preference selection binding; made currently selected preference appear on setting screen

--- app/src/main/java/tokyo/drift/reps/SettingsActivity.java
+++ app/src/main/java/tokyo/drift/reps/SettingsActivity.java
@@ -1,17 +1,48 @@
 package tokyo.drift.reps;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 /**
  * Created by josh on 27/02/2017.
  */
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
+
+        bindPreferenceValue(findPreference("units"));
     }
+
+    private void bindPreferenceValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(preference, PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                .getString(preference.getKey(), ""));
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference pref, Object value) {
+        String val = value.toString();
+
+        if (pref instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) pref;
+            int prefIndex = listPref.findIndexOfValue(val);
+
+            if (prefIndex >= 0) {
+                pref.setSummary(val);
+            }
+        }
+        return true;
+    }
+
+
+
 }
